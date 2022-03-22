cc911eb651a9ca68f7f615a286551d878a87dea3
Taisei Iwai <t.iwai.sys21@morijyobi.ac.jp>
2022-03-22 10:18:02 +0900

    四角形の面積計算機能追加

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import flask, render_template
+from flask import flask, render_template, request
 
 app = Flask(_name_)
 
 @app.route("/")
 def top_page():
     return render_template("index.html")
+
+@app.route("/square_input")
+def square_input():
+    return render_template("square_input.html")
+
+@app.route("/square_result")
+def square_result():
+    height = int(request.args.get("height"))
+    bottom = int(request.args.get("bottom"))
+    result = height * bottom
+    return render_template("spuare_result.html", result=result)
 
 if _name_ == "_main_":
     app.run(debug=true)
